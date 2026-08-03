ed18fff89e4900e7b0c075c7e2279f59b6f94ba3
AirKyzzZ <AirKyzzZ@users.noreply.github.com>
2026-08-03 14:15:58 +0200

    feat: request signed issuer metadata on OPENID4VCI_FINAL1

--- app/navigators/app-navigator.tsx
+++ app/navigators/app-navigator.tsx
@@ -131,6 +131,9 @@ const coreConfig = {
           encryption:
             '93d9182795f0d1bec61329fc2d18c4b4c1b7e65e69e20ec30a2101a9875fff7e',
         },
+        public: {
+          requestSignedMetadata: true,
+        },
       },
     },
     OPENID4VCI_FINAL1_HAIP: {
